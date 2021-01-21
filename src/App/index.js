a541e3bc8a6f38ed347a5cffb2bd58bbad8fee48
Osama-you <osama.w.younis@gmail.com>
2021-01-21 11:21:31 +0200

    Create Header Component - HTML & CSS is done - pass the handler func - add prop-type module - add search icon Relates #5

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,31 @@
+import { useState } from 'react';
+
 import './App.css';
+import Header from '../components/Header';
 
 function App() {
-  return <div className="App">hi app</div>;
+  const [searchValue, setSearchValue] = useState('');
+
+  const onSearchInputChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const onSubmitBtnClick = () => {
+    console.log(searchValue);
+
+    // clear Input after done
+    setSearchValue('');
+  };
+
+  return (
+    <div className="App">
+      <Header
+        searchInputValue={searchValue}
+        onSearchInputChange={onSearchInputChange}
+        onSubmitBtnClick={onSubmitBtnClick}
+      />
+    </div>
+  );
 }
 
 export default App;
